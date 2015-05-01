589553142e5d254a9babc77cb83403e83a554b4e
kendall@boardeffect.com <kendall@boardeffect.com>
2015-05-01 00:58:27 -0400

    base

---
+++ railsApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '9c8064cc4c645090a5abd44966130de967747b123f9a8084fb37315db63200f02901bba7b62951d0ccfc3aeb2b8847d9821e26166ad767459920bd99cd70cfd7'
